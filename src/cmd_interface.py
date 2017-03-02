91506172c9c0cf89f417c429cbf781df72fcd533
streambinder <posta@davidepucci.it>
2017-03-02 17:59:15 +0100

    src: cmd_interface: cleaner and more extensible code
    Planning to include commands to get hosts and services status.

--- src/cmd_interface.py
+++ src/cmd_interface.py
@@ -11,6 +11,19 @@ INTERVAL_DIALOGS_DISCOVER = 3  # second(s)
 INTERVAL_OVERALL_CYCLE = 2  # second(s)
 
 
+sock = None
+
+
+def telegram_command(command):
+    global sock
+    sock.send(command + "\r\n")
+    return sock.recv(4096)
+
+
+def telegram_message(message, peer):
+    return telegram_command("msg " + peer + " " + message)
+
+
 def parseable(message):
     if len(message) == 0:
         return False
@@ -19,17 +32,38 @@ def parseable(message):
         return False
     return True
 
-sock = None
+
+def command_ping():
+    return "All right. I'm still here."
+
+
+def command_ack(message):
+    if message.lower()[:3] == "ack":
+        message = message[3:].strip()
+    if len(message) == 0 or message.lower() == "help":
+        return "ack host_name [service_description]"
+    else:
+        params = message.split()
+        hostname = params[0].replace("\"", "").replace("'", "")
+        if len(params) > 1:
+            service = " ".join(params[1:]).replace("\"", "").replace("'", "").strip()
+            command = "[" + str(int(time.time())) + "] ACKNOWLEDGE_SVC_PROBLEM;" + hostname + ";" + service + ";1;1;1;nagiosadmin;Ack."
+            return_message = "Service \"" + service + "\" on \"" + hostname + "\" acknowledgment correctly fired."
+        else:
+            command = "[" + str(int(time.time())) + "] ACKNOWLEDGE_HOST_PROBLEM;" + hostname + ";1;1;1;nagiosadmin;Ack."
+            return_message = "Host \"" + hostname + "\" status acknowledgment correctly fired."
+        nagios_command_file = open(NAGIOS_COMMAND_FILE, 'w')
+        nagios_command_file.write(command + "\n")
+        nagios_command_file.close()
+        return return_message
 
 while True:
     if not sock:
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect(TELEGRAM_UNIX_SOCKET)
     try:
-        # print "Getting dialog list"
         unreads = set()
-        sock.send("dialog_list\r\n")
-        for entry in sock.recv(2048).split("\n"):
+        for entry in telegram_command("dialog_list").split("\n"):
             if len(entry) > 0:
                 if entry[:4].lower() == "user":
                     splitter = "User"
@@ -41,46 +75,22 @@ while True:
                 msgs = int(entry.split(":")[1].strip().split()[0])
                 if msgs > 0:
                     unreads.add(peer)
-        # print "Unread messages from: " + str(unreads) if len(unreads) > 0 else "No unread messages."
         time.sleep(INTERVAL_DIALOGS_DISCOVER)
         for peer in unreads:
-            sock.send("history " + peer + " " + str(int((INTERVAL_DIALOGS_DISCOVER + INTERVAL_OVERALL_CYCLE) / 5)) + "\r\n")
             message_id = 0
-            for line in sock.recv(4096).split("\n"):
+            for line in telegram_command("history " + peer + " " + str(int((INTERVAL_DIALOGS_DISCOVER + INTERVAL_OVERALL_CYCLE) / 5))).split("\n"):
                 message = line.replace("»»»", ">>>").strip()
                 message_id += 1
-                # print "ID: " + str(message_id) + "\tLength: " + str(len(message)) + "\tStatus: " + ("kept" if parseable(message) else "ignored") + "\tBody: \"" + message + "\""
                 if parseable(message):
                     message = message.split(">>>")[1].strip()
-                    if message.lower() == "ping":
-                        sock.send("msg " + peer + " All right. I'm still here.\r\n")
-                        sock.recv(1024)
-                    elif message.lower() == "ack help":
-                        sock.send("msg " + peer + " Quick ACK syntax: \"ack host_name [service_description]\".\r\n")
-                        sock.recv(1024)
+                    if len(message) >= 4 and message.lower()[:4] == "ping":
+                        telegram_message(command_ping(), peer)
+                    elif len(message) >= 3 and message.lower()[:3] == "ack":
+                        telegram_message(command_ack(message), peer)
                     else:
-                        # print "ID: " + str(message_id) + "\tMessage: " + message
-                        if "ack" in message.lower()[:3]:
-                            params = message.split()
-                            hostname = params[1].replace("\"", "").replace("'", "")
-                            service = None
-                            if len(params) > 2:
-                                service = " ".join(params[2:]).replace("\"", "").replace("'", "").strip()
-                            if service:
-                                command = "[" + str(int(time.time())) + "] ACKNOWLEDGE_SVC_PROBLEM;" + hostname + ";" + service + ";1;1;1;nagiosadmin;Ack."
-                                return_message = "Service \"" + service + "\" on \"" + hostname + "\" acknowledgment correctly fired."
-                            else:
-                                command = "[" + str(int(time.time())) + "] ACKNOWLEDGE_HOST_PROBLEM;" + hostname + ";1;1;1;nagiosadmin;Ack."
-                                return_message = "Host \"" + hostname + "\" status acknowledgment correctly fired."
-                            if command:
-                                # print "ID: " + str(message_id) + "\tCommand: " + command
-                                nagios_command_file = open(NAGIOS_COMMAND_FILE, 'w')
-                                nagios_command_file.write(command + "\n")
-                                nagios_command_file.close()
-                                sock.send("msg " + peer + " " + return_message + "\r\n")
-                                sock.recv(1024)
+                        telegram_message("Command not found.", peer)
     except Exception as e:
-        print "ERROR:" + str(e)
+        print "ERROR: " + str(e)
         try:
             sock.close()
         except:
