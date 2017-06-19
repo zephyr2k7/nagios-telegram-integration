3a26548a0b81b5a510f3366916b2721c229b7f36
streambinder <posta@davidepucci.it>
2017-06-19 17:57:30 +0200

    src: cmd_interface: smarter way to handle socket

--- src/cmd_interface.py
+++ src/cmd_interface.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import select
 import socket
 import time
 
@@ -58,42 +59,48 @@ def command_ack(message):
         return return_message
 
 while True:
-    if not sock:
+    time.sleep(INTERVAL_OVERALL_CYCLE)
+    if sock is None:
         sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         sock.connect(TELEGRAM_UNIX_SOCKET)
     try:
-        unreads = set()
-        for entry in telegram_command("dialog_list").split("\n"):
-            if len(entry) > 0:
-                if entry[:4].lower() == "user":
-                    splitter = "User"
-                elif entry[:4].lower() == "chat":
-                    splitter = "Chat"
-                else:
-                    continue
-                peer = str(entry.split(":")[0].split(splitter)[1].strip().replace(" ", "_"))
-                msgs = int(entry.split(":")[1].strip().split()[0])
-                if msgs > 0:
-                    unreads.add(peer)
-        time.sleep(INTERVAL_DIALOGS_DISCOVER)
-        for peer in unreads:
-            message_id = 0
-            for line in telegram_command("history " + peer + " " + str(int((INTERVAL_DIALOGS_DISCOVER + INTERVAL_OVERALL_CYCLE) / 5))).split("\n"):
-                message = line.replace("»»»", ">>>").strip()
-                message_id += 1
-                if parseable(message):
-                    message = message.split(">>>")[1].strip()
-                    if len(message) >= 4 and message.lower()[:4] == "ping":
-                        telegram_message(command_ping(), peer)
-                    elif len(message) >= 3 and message.lower()[:3] == "ack":
-                        telegram_message(command_ack(message), peer)
-                    else:
-                        telegram_message("Command not found.", peer)
-    except Exception as e:
-        print "ERROR: " + str(e)
+        ready_r, ready_w, ready_err = \
+            select.select([sock, ], [sock, ], [], 5)
+    except select.error:
         try:
+            sock.shutdown(2)
             sock.close()
         except:
             pass
-        sock = None
-    time.sleep(INTERVAL_OVERALL_CYCLE)
+        finally:
+            sock = None
+        print "ERROR: " + str(e)
+        continue
+
+    unreads = set()
+    for entry in telegram_command("dialog_list").split("\n"):
+        if len(entry) > 0:
+            if entry[:4].lower() == "user":
+                splitter = "User"
+            elif entry[:4].lower() == "chat":
+                splitter = "Chat"
+            else:
+                continue
+            peer = str(entry.split(":")[0].split(splitter)[1].strip().replace(" ", "_"))
+            msgs = int(entry.split(":")[1].strip().split()[0])
+            if msgs > 0:
+                unreads.add(peer)
+    time.sleep(INTERVAL_DIALOGS_DISCOVER)
+    for peer in unreads:
+        message_id = 0
+        for line in telegram_command("history " + peer + " " + str(int((INTERVAL_DIALOGS_DISCOVER + INTERVAL_OVERALL_CYCLE) / 5))).split("\n"):
+            message = line.replace("»»»", ">>>").strip()
+            message_id += 1
+            if parseable(message):
+                message = message.split(">>>")[1].strip()
+                if len(message) >= 4 and message.lower()[:4] == "ping":
+                    telegram_message(command_ping(), peer)
+                elif len(message) >= 3 and message.lower()[:3] == "ack":
+                    telegram_message(command_ack(message), peer)
+                else:
+                    telegram_message("Command not found.", peer)
